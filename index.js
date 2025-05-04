ad980e2f2629850fcd00275fcba76651464043d5
RoeyTamano <roitamano@gmail.com>
2025-05-05 01:39:50 +0300

    build api

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let dateParam = req.params.date;
+  let date = dateParam ? !isNaN(dateParam) ? new Date(parseInt(dateParam)) :
+  new Date(dateParam) : new Date()
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date"});
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+  
 });
 
 
